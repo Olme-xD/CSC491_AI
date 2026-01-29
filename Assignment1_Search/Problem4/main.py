31ee01e8ec5c91f439fa8b431c607000c9b4b036
Olme-xD <olmem.molina02@gmail.com>
2026-01-29 09:15:48 -0500

    problem 4 and 5 answers

--- Assignment1_Search/Problem4/main.py
+++ Assignment1_Search/Problem4/main.py
@@ -15,16 +15,11 @@ _minimax.draw_state(state)
 current_player = _minimax.player(state)
 print(f"\nPlayer turn: {current_player}")
 best_score = _minimax.max_value(state)
-print(f"Possible outcomes for X: {best_score}")
+print(f"Best possible outcomes for X: {best_score}")
 
 # Answer for a
 _minimax.print_tree(state)
-
 # Answer for b
 print(f"Space complexity (including root): {_minimax.max_depth + 1}")
-
 # Answer for c
 print(f"Number of terminal states: {_minimax.terminalS_count}")
-
-# Asnwer for d
-
